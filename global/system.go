76768b3a61c8fe035f5c908637599a69c124a81a
gaoke966 <gaoke966@qq.com>
2020-01-28 21:53:08 +0800

    update desc

--- global/system.go
+++ global/system.go
@@ -2,5 +2,5 @@ package global
 
 const (
 	SYSTEM_VERSION   = "v0.1.5"              // system version code
-	SYSTEM_COPYRIGHT = "2018 - 2019 phachon" // system copyright
+	SYSTEM_COPYRIGHT = "2020" // system copyright
 )
